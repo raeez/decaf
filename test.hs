663f10bd8a37fea3da4c13c7bad33979f686afe9
Justin Zhang <zhunping@csail.mit.edu>
2011-02-24 20:36:46 -0500

    starting to adding tests for the semantics checkers
     Please enter the commit message for your changes. Lines starting

--- test.hs
+++ test.hs
@@ -145,3 +145,60 @@ arithmeticExpressions = TestList [
   TestLabel "simple-expressions" (readExpressions "3+2*2 3*5+6 21/3-2" ~=? Success [7,21,5]),
   TestLabel "paren-expressions" (readExpressions "(3+6*(2-11)) (21+21/21)/22" ~=? Success [-51, 1])
   ]
+
+
+
+--------------------------------------------
+-- Semantics Checking Test
+--
+
+
+
+{-
+1. No identifier is declared twice in the same scope.
+
+class Program {
+  void main() {
+    int x;
+    boolean x;  // identifier declared twice
+  }
+}
+
+
+
+2. No identifier is used before it is declared.
+3. The program contains a definition for a method called main that has no parameters (note
+that since execution starts at method main, any methods defined after main will never be
+executed).
+4. The <int literal> in an array declaration must be greater than 0.
+5. The number and types of arguments in a method call must be the same as the number and
+types of the formals, i.e., the signatures must be identical.
+6. If a method call is used as an expression, the method must return a result.
+7. A return statement must not have a return value unless it appears in the body of a method
+that is declared to return a value.
+8. The expression in a return statement must have the same type as the declared result type
+of the enclosing method definition.
+9. An <id> used as a <location> must name a declared local/global variable or formal parameter.
+10. For all locations of the form <id>[<expr>]
+(a) <id> must be an array variable, and
+(b) the type of <expr> must be int.
+11. The <expr> in an if statement must have type boolean.
+12. The operands of <arith op>s and hrel opis must have type int.
+13. The operands of <eq op>s must have the same type, either int or boolean.
+14. The operands of <cond op>s and the operand of logical not (!) must have type boolean.
+15. The <location> and the <expr> in an assignment, <location> = <expr>, must have the same type.
+16. The <location> and the <expr> in an incrementing/decrementing assignment, <location> += <expr>
+and <location> -= <expr>, must be of type int.
+17. The initial <expr> and the ending <expr> of for must have type int.
+18. All break and continue statements must be contained within the body of a for.
+
+
+
+
+
+
+
+
+
+
+-}
