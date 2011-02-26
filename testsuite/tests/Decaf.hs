aaeea01b65ef9977d3254ab9343907e2a8742f36
jzz <gnipnuhz@gmail.com>
2011-02-26 13:00:12 -0500

    restructured tests structure

---
+++ testsuite/tests/Decaf.hs
@@ -0,0 +1,14 @@
+module Decaf (
+
+    module Decaf.Main,
+    module Decaf.Tokens,
+    module Decaf.AST,
+    module Decaf.Scanner,
+    module Decaf.Parser,
+
+ ) where
+
+import Decaf.Tokens
+import Decaf.AST
+import Dacaf.Scanner
+import Decaf.Parser
