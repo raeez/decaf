c2470ae80f7a04b2c37ccd83bc14de057dd36573
Raeez Lorgat <raeez@mit.edu>
2011-02-27 13:01:07 -0500

    tree grapher binary

--- src/GrapherBin.hs
+++ src/GrapherBin.hs
@@ -4,70 +4,28 @@ import Data.Graph.Inductive
 import Data.GraphViz
 import Decaf.Parser
 import Decaf.AST
+import System.Process
+import System.Environment
+import System.Exit
+import System.Random
+import Data.List
 
 main :: IO ()
 main = do
-        putStrLn "Decaf Parser REPL"
-        putStrLn "Enter your input program at the prompt\n"
-        replEval
+        args <- getArgs
+        case args of
+          [inputFile] -> (readFile inputFile) >>= parse inputFile
+          otherwise -> putStrLn "error, must specify the input file as the first and only arg"
 
-replEval :: IO ()
-replEval = do
-            inp <- getLine
-            case ps expr inp of
-              RSuccess a -> (putStrLn $ printDotGraph $ graphToDot graphParams ( graph a)) >> (putStrLn $ "\n\n" ++ (put_expr 0 a))
-              RError e -> putStrLn $ "Error:\n" ++ e
-            replEval
-
-graph d = let (nodes, edges) = convertTree d in
-              mkGraph nodes edges :: Gr String String
-
-convertTree d  = ( [    (1, "Collect underpants"),
-                        (3, "Profit")   ],           [ (1, 3, "?") ] )
-class GraphViz a where
-    toGraphViz :: a -> ((Int, String), [(Int, Int, String)])
-
---instance DecafExpr where
-    --toGraphViz n (DecafExpr term expr') = ([this] ++ children, leaves)
-                                          --where
-                                            --this = (n, 
-                                            --children = []
-                                            --leaves = 
---instance Expr' where
-    --toGraphViz n (Expr' term expr') = ([this] ++ children, leaves) 
-                                      --where
-                                          --children = 
-                                          --leaves = 
-
---flatten n (DecafExpr term expr') = []
---flatten n (Expr' binop term expr') = []
---flatten n (EmptyExpr') = []
---flatten n (Term factor term') = []
---flatten n (Term' binop factor term) = []
---flatten n (EmptyTerm') = []
---flatten n (DecafParenExpr' de) = []
---flatten n (DecafNotExpr' dne) = []
---flatten n (DecafMinExpr' dme) = []
---flatten n (DecafLocExpr' dle) = []
---flatten n (DecafMethodExpr' dme) = []
---flatten n (DecafLitExpr' dle) = []
---flatten n (DecafBinArithOp dbo) = []
---flatten n (DecafBinRelOp dbo) = []
---flatten n (DecafBinEqOp dbo) = []
---flatten n (DecafBinCondOp dco) = []
---flatten n (DecafPlusOp dpo) = []
---flatten n (DecafMinOp dmo) = []
---flatten n (DecafMulOp dmo) = []
---flatten n (DecafDivOp dvo) = []
---flatten n (DecafModOp dmo) = []
---flatten n (DecafLTOp dlto) = []
---flatten n (DecafGTOp dgto) = []
---flatten n (DecafLTEOp dlteo) = []
---flatten n (DecafGTEOp dgteo) = []
---flatten n (DecafEqOp deo) = []
---flatten n (DecafNEqOp dneo) = []
---flatten n (DecafAndOp dao) = []
---flatten n (DecafOrOp doo) = []
+parse inputFile input = do
+          case ps program input of
+              RSuccess a -> putStrLn (show a) >> outputGraph a >> convertToPs >> exitSuccess
+              RError e -> putStrLn ("Error:\n" ++ (show e)) >> exitFailure
+          where
+            outputFile = inputFile ++ ".out"
+            graphText =  printDotGraph . (graphToDot graphParams) . buildGraph
+            outputGraph a = (putStrLn $ graphText a) >> (writeFile outputFile $ graphText a)
+            convertToPs = (runCommand $ "dot -Tpng " ++ outputFile ++ " -o" ++ outputFile ++ ".png") >> (return $ putStrLn "")
 
 graphParams :: GraphvizParams String String () String
 graphParams = nonClusteredParams {
@@ -78,11 +36,9 @@ graphParams = nonClusteredParams {
   where
     ga = [
       GraphAttrs [
-        RankDir FromLeft,
         (BgColor . X11Color) Transparent
         ],
       NodeAttrs [
-        Shape BoxShape,
         (FillColor . X11Color) White,
         Style [SItem Filled []]
         ]
@@ -90,3 +46,62 @@ graphParams = nonClusteredParams {
 
     fn (n,l) = [(Label . StrLabel) l]
     fe (f,t,l) = [(Label . StrLabel) l]
+
+type Table a = [a]
+
+ --numberTree :: Eq a => Tree a -> State (Table a) (Tree Int)
+ --numberTree Nil = return Nil
+ --numberTree (Node val@(i,v) t1 t2) =  do
+                                        --nv <- numberNode val
+                                        --nt1 <- numberTree t1
+                                        --nt2 <- numberTree t2
+                                        --return (Node nv nt1 nt2)
+                                      --where
+                                        --numberNode :: Eq a => a -> State (Table a) Int -- must return a tuple of (id, val)
+                                        --numberNode val@(i, v) = do 
+                                                                  --table <- get
+                                                                  --(newTable, numberedNode) <- return (nNode val table)
+                                                                  --put newTable
+                                                                  --return numberedNode
+
+                                        --nNode::  (Eq a) => a -> Table a -> (Table a, Int)
+                                        --nNode val@(i, v) table = (table ++ [val], length table)
+
+ --numTree :: (Eq a) => Tree a -> Tree Int
+ --numTree t = evalState (numberTree t) []
+ --testTree = Node "Zero" (Node "One" (Node "Two" Nil Nil) (Node "One" (Node "Zero" Nil Nil) Nil)) Nil
+ --numTree testTree => Node 0 (Node 1 (Node 2 Nil Nil) (Node 1 (Node 0 Nil Nil) Nil)) Nil
+
+
+
+buildGraph d = mkGraph nodes edges :: Gr String String
+               where
+                  (nodes, edges) = ([], [])
+-- convert AST -> Tree a b
+-- number tree
+-- convert numebered tree to graph
+--
+data Tree a b = Node (a, b) (Maybe [Tree a b])
+              | Nil
+              deriving (Eq, Show)
+
+pretty_var (DecafVar ty ident) = "Var("++ show ty ++ "|" ++ show ident ++ ")"
+pretty_arr (DecafArr ty ident len) = "Arr(" ++ show ty ++ "|" ++ show ident ++"["++show len ++ "])"
+pretty_meth (DecafMethod ty ident args body) = "Method(" ++ show ty ++ "|" ++ show ident ++ "|" ++ show args ++ ")"
+
+convertProgram :: DecafProgram -> Tree Int String
+convertProgram p  = Node (0, "Program") (Just children)
+                    where
+                      fs = map convertField (fields p)
+                      ms = map convertMethod (methods p)
+                      children = fs ++ ms
+
+convertField :: DecafField -> Tree Int String
+convertField (DecafVarField var@(DecafVar ty ident)) = Node (0, pretty_var var) Nothing
+convertField (DecafArrField arr@(DecafArr ty ident len)) = Node (0, pretty_arr arr) Nothing
+
+convertMethod :: DecafMethod -> Tree Int String
+convertMethod meth@(DecafMethod ty ident args body) = Node(0, pretty_meth meth) (Just $ convertBlock body)
+
+convertBlock :: DecafBlock -> [Tree Int String]
+convertBlock (DecafBlock vars stms) = [Nil]
