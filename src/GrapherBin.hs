256c74fa47e3d758c5b577410e418e6e258220c2
Raeez Lorgat <raeez@mit.edu>
2011-02-27 13:35:33 -0500

    first few levels of tree rewrite implemented

--- src/GrapherBin.hs
+++ src/GrapherBin.hs
@@ -7,7 +7,6 @@ import Decaf.AST
 import System.Process
 import System.Environment
 import System.Exit
-import System.Random
 import Data.List
 
 main :: IO ()
@@ -19,7 +18,7 @@ main = do
 
 parse inputFile input = do
           case ps program input of
-              RSuccess a -> putStrLn (show a) >> outputGraph a >> convertToPs >> exitSuccess
+              RSuccess a -> putStrLn (show a) >> outputGraph a >> convertToPs >> (putStrLn $ show $ convertProgram a) >> exitSuccess
               RError e -> putStrLn ("Error:\n" ++ (show e)) >> exitFailure
           where
             outputFile = inputFile ++ ".out"
@@ -88,7 +87,8 @@ data Tree a b = Node (a, b) (Maybe [Tree a b])
 pretty_var (DecafVar ty ident) = "Var("++ show ty ++ "|" ++ show ident ++ ")"
 pretty_arr (DecafArr ty ident len) = "Arr(" ++ show ty ++ "|" ++ show ident ++"["++show len ++ "])"
 pretty_meth (DecafMethod ty ident args body) = "Method(" ++ show ty ++ "|" ++ show ident ++ "|" ++ show args ++ ")"
-
+pretty_op (a) = show a
+pretty_arr_expr(DecafArrLoc ident expr) = "Arr(" ++ show ident ++ "[])"
 convertProgram :: DecafProgram -> Tree Int String
 convertProgram p  = Node (0, "Program") (Just children)
                     where
@@ -97,11 +97,57 @@ convertProgram p  = Node (0, "Program") (Just children)
                       children = fs ++ ms
 
 convertField :: DecafField -> Tree Int String
-convertField (DecafVarField var@(DecafVar ty ident)) = Node (0, pretty_var var) Nothing
+convertField (DecafVarField var@(DecafVar ty ident)) = convertVar var
 convertField (DecafArrField arr@(DecafArr ty ident len)) = Node (0, pretty_arr arr) Nothing
+
+convertVar :: DecafVar -> Tree Int String
+convertVar var@(DecafVar ty ident) = Node (0, pretty_var var) Nothing
 
 convertMethod :: DecafMethod -> Tree Int String
 convertMethod meth@(DecafMethod ty ident args body) = Node(0, pretty_meth meth) (Just $ convertBlock body)
 
 convertBlock :: DecafBlock -> [Tree Int String]
-convertBlock (DecafBlock vars stms) = [Nil]
+convertBlock (DecafBlock vars stms) = vs ++ ss
+                                      where
+                                        vs = map convertVar vars
+                                        ss = map convertStm stms
+
+convertStm :: DecafStm -> Tree Int String
+convertStm (DecafAssignStm loc op expr) = Node (0, pretty_op op) (Just $ [convertLoc loc] ++ [convertExpr expr])
+convertStm (DecafMethodStm (DecafPureMethodCall ident args)) = Node (0, "PureMethodcall") (Just $ [convertIdent ident] ++ (map convertExpr args))
+convertStm (DecafMethodStm (DecafMethodCallout ident args)) = Node (0, "MethodCallOut") (Just $ [convertStr ident] ++ (map convertCalloutArg args))
+convertStm (DecafIfStm expr block elseblock) = Node (0, "if") (Just $ [convertExpr expr] ++ convertBlock block ++ elseblock')
+                                              where
+                                                elseblock' = case convertElseBlock elseblock of
+                                                                Just a -> a
+                                                                Nothing -> []
+convertStm (DecafForStm ident expr expr' block) = Node (0, "for") (Just $ [convertIdent ident] ++ [convertExpr expr] ++ [convertExpr expr'] ++ convertBlock block)
+convertStm (DecafRetStm expr) = Node (0, "ret") $ convertRetExpr expr
+convertStm (DecafBreakStm) = Node (0, "brk") Nothing
+convertStm (DecafContStm) = Node (0, "cnt") Nothing
+convertStm (DecafBlockStm block) = Node (0, "blockStm") (Just $ convertBlock block)
+
+convertRetExpr :: (Maybe DecafExpr) -> (Maybe [Tree Int String])
+convertRetExpr (Just expr) = Just $ [convertExpr expr]
+convertRetExpr Nothing = Nothing
+
+convertElseBlock :: (Maybe DecafBlock) -> (Maybe [Tree Int String])
+convertElseBlock (Just block) = Just $ convertBlock block
+convertElseBlock Nothing = Nothing
+
+convertCalloutArg :: DecafCalloutArg -> Tree Int String
+convertCalloutArg (DecafCalloutArgExpr expr) = convertExpr expr
+convertCalloutArg (DecafCalloutArgStr str) = convertStr str
+
+convertIdent :: DecafIdentifier -> Tree Int String
+convertIdent (DecafIdentifier str) = Node(0, "IDENT("++ str ++")") Nothing
+
+convertLoc :: DecafLoc -> Tree Int String
+convertLoc var@(DecafVarLoc ident) = convertIdent ident
+convertLoc arr@(DecafArrLoc ident expr) = Node (0, pretty_arr_expr arr) (Just $ [convertExpr expr])
+
+convertExpr :: DecafExpr -> Tree Int String
+convertExpr expr = Nil --fix
+
+convertStr :: DecafString -> Tree Int String
+convertStr (DecafString str) = Node (0, "STR("++str++")") Nothing
