1173b2b9aa89313918c820fbb4d858059d0ad586
Scott Kovach <scott@Edwin.(none)>
2011-02-27 20:51:47 -0500

    Added statement checkers, have not debugged

--- src/Decaf/Checker.hs
+++ src/Decaf/Checker.hs
@@ -9,7 +9,7 @@ import Decaf.AST
 data SymbolRecord = VarRec {symbol :: DecafVar }
                   | MethodRec {symbol :: DecafMethod}
                   | ArrayRec {symbol :: DecafArray}
-                    deriving Show 
+                    deriving (Show,Eq)
 
 
 -- access just the ID, for declaration checking
@@ -23,9 +23,15 @@ symType (MethodRec m) = methodType m
 symType (ArrayRec a) = arrayType a
 
 
-newtype SymbolTable = SymbolTable {symbolRecords :: [SymbolRecord]}
-    deriving Show
+newtype SymbolTable = SymbolTable {symbolRecords :: [SymbolRecord]
+                                  , blockType :: BlockType}
+    deriving Show, Eq
+
+data BlockType = ForBlock | IfBlock | MethodBlock DecafType | GlobalBlock | TrivialBlock
+                 deriving (Show,Eq)
+
 type SymbolTree = Tree SymbolTable
+
 
 newtype Checker a = Checker {runChecker :: (String, SymbolTree) -> (a, (String, SymbolTree))}
 
@@ -35,51 +41,173 @@ instance Monad Checker where
     return a = Checker (\s -> (a, s))
     m >>= f = Checker (\s -> let (a,(e,t)) = runChecker m s in runChecker (f a) (e,t))
 
-pushError str = Checker (\(e,t) -> ((),(e++str++"\n",t)))
-addSymbol id = Checker (\(e,t) -> ((), (e, modifyTreeCnt g t))) 
-    where g st = SymbolTable ((VarRec id) : (symbolRecords st))
-local m = Checker (\(e,t)-> let (a,(e',t')) = runChecker m (e, addChild (SymbolTable []) t) in
+pushError :: String -> Checker ()
+pushError str = (Checker (\(e,t) -> ((),(e++str++"\n",t)))) >> (return False)
+
+addSymbol :: SymbolRecord -> Checker ()
+addSymbol sr = Checker (\(e,t) -> ((), (e, modifyTreeCnt g t))) 
+    where g (SymbolTable rs bt) = SymbolTable (sr : rs) bt
+
+local :: BlockType -> Checker a -> Checker a
+local tp m = Checker (\(e,t)-> let (a,(e',t')) = runChecker m (e, addChild (SymbolTable [] tp) t) in
                             (a, (e', setContext (context t) t')))
+getST :: Checker SymbolTree
 getST  = Checker (\(e,t) -> (t,(e,t)))
 
+lookNear :: DecafIdentifier -> Checker SymbolRecord
 lookNear id = do st <- getST
                  let recs = symbolRecords.getContent $ st
-                 return (id `elem` (map iden recs))
+                 return (find id (zip (map symID recs) recs))
 
+lookFar :: DecafIdentifier -> Checker SymbolRecord
 lookFar id = do st <- getST
                 return $ exists st
     where exists st = let recs = symbolRecords.getContent $ st in
-                      id `elem` (map iden recs) ||
-                         if isRoot st
-                         then False
-                         else exists $ parent st
+                      case (find id (zip (map symID recs) recs)) of
+                        Nothing -> if isRoot st
+                                   then Nothing
+                                   else (exists $ parent st)
+                        other -> other
 
+inFor = do st <- getST
+           return infor st
 
+    where infor st =
+              if (blockType.getContent.node $ st) == ForBlock
+              then True
+              else if isRoot st
+                   then False
+                   else inFor $ parent st)
+
+inMethod :: Checker DecafType
+inMethod = do st <- getST
+              return $ inmethod st
+    where inmethod st = 
+              case blockType.getContent.node $ st of
+                MethodBlock t -> Just t
+                other -> if isRoot st
+                         then Nothing
+                         else inmethod $ parent st
 
 type Identifier = String
 
-checkStatement stmt = 
-    case stmt of
-      Ass t s -> do {
-                   x <- lookFar t;
-                   y <- lookFar s;
-                   if x
-                   then if y
-                        then pushError ("Assignment: "++t++"="++s) >> return True
-                        else pushError ("Undefined variable "++s) >> return False
-                   else pushError ("Undefined variable "++t) >> return False
-                 }
-      Dec id -> lookNear id >>= \b ->
-                if b 
-                then pushError ("Repeat Declaration "++id) >> return False
-                else pushError ("Declaration: " ++ id) >> addSymbol id >> return True
-      Expr id -> lookFar id >>= \b ->
-                 if b 
-                 then pushError ("Expression: "++id) >> return True 
-                 else pushError ("Undefined expression "++id) >> return False
-      block@(Block _) -> do pushError "Block"
-                            local (foldl (>>) (return True) (map checkStatement (contents block)))
-                            return True -- change this
+{- to write : 
+
+  getLocType -- location type
+  checkExpr  -- returns Maybe DecafType
+ 
+
+error pushing code needs line numbers
+  
+
+-}
+checkStm :: DecafStm -> Checker Bool
+checkStm stmt = 
+    case stmt of 
+      DecafAssignStm loc op expr -> 
+          do lhs <- lookFar (decafID loc)
+             t2 <- checkExpr expr
+             let t1 = symID lhs
+             case op of
+               DecafEq -> if (t1 == t2)
+                          then return True
+                          else pushError ("Mismatched types in assignment statement") 
+                                   >> return False
+               other -> if (t1 == DInteger && t2 == DInteger)
+                        then return True
+                        else pushError ("Mismatched types in arithmetic assignment statement") 
+                                 >> return False
+      DecafMethodStm (DecafPureMethodCall dID args) -> 
+          do ex <- lookFar dID
+             case ex of 
+               Just (MethodRec decafMethod) 
+                   -> do types <- bindCat [] (map checkExpr args)
+                         if all (== True) $ zipWith (==) types (map varType (methodArg decafMethod))
+                         then return True
+                         else pushError ("Incorrect argument type")
+               Nothing -> pushError ("Undeclared method: " ++ dId)
+          where 
+            bindCat :: [DecafType] -> [Checker DecafType] -> Checker [DecafType]
+            bindCat res args = 
+                case args of
+                  [] -> return res
+                        other -> do t <- (head args)
+                                    bindCat (res++t) (tail args)
+
+      DecafIfStm expr block melse ->
+          do case checkExpr expr of
+               Just dt -> if dt == DBool
+                          then return ()
+                          else pushError ("If conditional must have type Boolean")
+               Nothing -> return ()
+
+             case melse of
+               Just e -> checkBlock block IfBlock >> checkBlock e IfBlock
+               Nothing -> checkBlock block IfBlock
+
+      DecafForStm id expr1 expr2 block -> 
+          do addSymbol $ VarRec $ DecafVar DInteger id
+             t1 <- checkExpr expr1
+             t2 <- checkExpr expr2
+             if (t1 /= DInteger)
+             then pushError ("Start expression in for loop must have type Integer")
+             else return ()
+             if (t2 /= DInteger)
+             then pushError ("End expression in for loop must have type Integer")
+             else return ()
+             checkBlock block ForBlock
+
+      DecafRetStm rv -> 
+          do t <- inMethod -- returns method type, if there is one
+                  case t of
+                    None -> pushError ("return may only be used inside a method definition")
+                    Just DVoid -> 
+                        case rv of
+                          None -> return True
+                          other -> pushError ("Function declared void cannot return a value")
+                    Just x ->
+                        case rv of
+                          None -> pushError ("Function declared to return a value must do so")
+                          Just expr -> do rt <- checkExpr expr
+                                          if rt == x
+                                          then return True
+                                          else pushError ("Function declared to return "++x++" cannot return "++rt)
+                                                      
+
+      DecafBreakStm -> guardFor
+      DecafContStm  -> guardFor
+          where guardFor = do b <- inFor
+                              if b
+                              then return True
+                              else pushError ("break and continue may only be used inside for loops")
+      BlockStm block -> checkBlock block TrivialBlock
+
+
+checkBlock :: DecafBlock -> BlockType -> Checker Bool
+checkBlock (DecafBlock vars stmts) btype 
+    = do (foldl (>>) (return False) (map checkVarDec vars))
+         (foldl (>>) (return False) (map checkStm stmts))
+
+
+
+checkVarDec :: DecafVar -> Checker Bool
+checkVarDec (DecafVar t id) = 
+    do rec <- lookNear id
+       case rec of 
+         Nothing -> addSymbol $ VarRec $ DecafVar t id
+         other -> pushError ("Variable "++id++" already defined at line number ")
+
+
+checkFieldDec :: DecafField -> Checker Bool
+checkFieldDec (DecafVarField var) = checkVarDec var
+checkFieldDec (DecavArrField (DecafArray t id len)) = 
+    do if len < 0 -- needs to be fixed
+
+
+                          
+          
+
+      
 
 
 ---- TEMPORARY PARSING FUNCTION -----
@@ -220,7 +348,7 @@ main = do args <- getArgs
           str <- readFile "chinput.txt"
           case parse parseStmt "" str of
             Right val -> 
-                case (runChecker (checkStatement val) ("", mkTree $SymbolTable [])) of
+                case (runChecker (checkStm val) ("", mkTree $SymbolTable [])) of
                   (_,(e,t)) -> putStrLn $ e ++ (show t)
             Left err -> putStrLn.show $ err
 
