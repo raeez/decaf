db6e447d1f50689a1bab8da6585e4179ffc77e7c
Raeez Lorgat <raeez@mit.edu>
2011-03-04 21:13:37 -0200

    updated lib module definition to recognize latest modules

--- src/Decaf.hs
+++ src/Decaf.hs
@@ -5,10 +5,20 @@ module Decaf (
     module Decaf.AST,
     module Decaf.Scanner,
     module Decaf.Parser,
+    module Decaf.Checker,
+    module Decaf.Data.ContextTree,
+    module Decaf.Data.SymbolTable
+    module Decaf.Data.Tree
+    module Decaf.Util
 
  ) where
 
+import Decaf.Util
+import Decaf.Data.ContextTree
+import Decaf.Data.Tree
+import Decaf.Data.SymbolTable
 import Decaf.Tokens
 import Decaf.AST
 import Dacaf.Scanner
 import Decaf.Parser
+import Decaf.Checker
