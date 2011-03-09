bac38059fd4ed326f53b0abf2b6e950111e1c280
Raeez Lorgat <raeez@mit.edu>
2011-03-09 16:09:26 -0500

    updated interface to checkFile function in checker binary

--- src/CheckerBin.hs
+++ src/CheckerBin.hs
@@ -1,6 +1,7 @@
 module Main where
 import System.Environment
 import System.Exit
+import Decaf.Util.Report
 import Decaf.Checker
 
 main :: IO ()
@@ -8,21 +9,16 @@ main = do args <- getArgs
           case args of
             [s, f] | s == "-debug" ->
                     do str <- readFile f
-                       let output = checkFile str f
-                       putStrLn.fst $ output
-                       checkOutput output
-
-            [f] ->
-                do str <- readFile f
-                   let output = checkFile str f
-                   if not (null (snd output))
-                    then (putStrLn.snd $ output) >> exitFailure
-                    else exitSuccess
-
+                       check True str f
+            [f] -> do str <- readFile f
+                      check False str f
             _ -> putStrLn "Invalid command line input" >> exitFailure
 
-checkOutput :: (String, String) -> IO ()
-checkOutput output =
-    if length (snd output) > 0
-    then exitSuccess
-    else exitFailure
+check :: Bool -> String -> String -> IO ()
+check d input inputFile = do
+        case checkFile input inputFile of
+          RSuccess (_, _, dT, dE) -> printDebug dT >> putStrLn dE >> exitSuccess
+          RError e -> putStrLn ("Error:\n" ++ show e) >> exitFailure
+        where
+          printDebug t = do
+                          if d then putStrLn t else return ()
