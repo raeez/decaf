f6f59f8a7da20e9725e58930c1e4e3242719a115
Raeez Lorgat <raeez@mit.edu>
2011-03-09 13:13:28 -0500

    Zipper utility functions

--- src/Decaf/Data/Zipper.hs
+++ src/Decaf/Data/Zipper.hs
@@ -1,26 +1,60 @@
 module Decaf.Data.Zipper where
 
-data Tree a = Node a [Tree a]
-            deriving (Eq, Show)
+data Tree a = Node {
+    content :: a,
+    children ::[Tree a]
+  } deriving (Eq)
 
 data Context a = Root
                | Child Int (Tree a) (Context a)
-               deriving (Eq, Show)
+               deriving (Eq)
 
-type Location a = (Tree a, Context a)
+type Zipper a = (Tree a, Context a)
 
-select :: Int -> Location a -> Location a
+select :: Int -> Zipper a -> Zipper a
 select i (t@(Node val children), ctx) = (children !! i, Child i t ctx)
 
-top :: Tree a -> Location a
+top :: Tree a -> Zipper a
 top t = (t, Root)
 
-up :: Location a -> Location a
-up (tree, (Child n (Node val children) c)) = (Node val (take n children ++ [tree] ++ drop n children), c)
+parent :: Zipper a -> Zipper a
+parent (tree, Child n (Node val children) c) = (Node val (take n children ++ [tree] ++ drop n children), c)
 
-upmost :: Location a -> Location a
-upmost l@(t, Root) = l
-upmost l = upmost (up l)
+root :: Zipper a -> Zipper a
+root l@(t, Root) = l
+root l = root (parent l)
 
-modify :: (Tree a -> Tree a) -> Location a -> Location a
+modify :: (Tree a -> Tree a) -> Zipper a -> Zipper a
 modify f (t, c) = (f t, c)
+
+-- implement
+--setContext :: Context -> ContextTree a -> ContextTree a
+--setContext c = move (\_ -> c)
+
+isRoot :: Zipper a -> Bool
+isRoot (_, Root) = True
+isRoot _ = False
+
+addChild :: a -> Zipper a -> Zipper a
+addChild v z = modify addchild z
+    where addchild (Node val children) = Node val (children ++ [Node v []])
+
+modifyContent :: (a -> a) -> Zipper a -> Zipper a
+modifyContent f = modify changecontent
+    where changecontent (Node val children) = Node (f val) children
+
+getContent :: Zipper a -> a
+getContent (Node val children, _) = val
+
+mkZipper :: a -> Zipper a
+mkZipper val = top (Node val [])
+
+instance (Show a) => Show (Tree a) where
+    show node = display 0 node
+        where
+            display indent node = spaces indent ++ Prelude.show (content node) ++ "\n" ++ concatMap (display $ indent+1) (children node)
+            spaces indent = map (\_ -> ' ') [1..3*indent]
+
+instance (Show a) => Show (Context a) where
+    show Root = "ROOT"
+    show (Child i t c) = "CHILD " ++ Prelude.show i ++ " <" ++ show c ++ ">"
