8b4eead916fe1b12c1bf72cfe78e9e95492ac3c7
Scott Kovach <scott@Edwin.(none)>
2011-03-11 16:41:29 -0500

    Made most Decaf modules publicly available

--- src/Decaf.hs
+++ src/Decaf.hs
@@ -1,24 +1,28 @@
-module Decaf (
+module Decaf 
+    ( module Decaf.Tokens
+    , module Decaf.AST
+    , module Decaf.Scanner
+    , module Decaf.Parser
+    , module Decaf.Checker
+    , module Decaf.Data.Zipper
+    , module Decaf.Data.SymbolTable
+    , module Decaf.Data.Tree
+--    module Decaf.Data.ContextTree,
 
-    module Decaf.Main,
-    module Decaf.Tokens,
-    module Decaf.AST,
-    module Decaf.Scanner,
-    module Decaf.Parser,
-    module Decaf.Checker,
-    module Decaf.Data.ContextTree,
-    module Decaf.Data.SymbolTable,
-    module Decaf.Data.Tree,
-    module Decaf.Util.Report
+
+
+--    module Decaf.Util.Report
 
  ) where
 
+
 import Decaf.Util.Report
-import Decaf.Data.ContextTree
+--import Decaf.Data.ContextTree
 import Decaf.Data.Tree
 import Decaf.Data.SymbolTable
 import Decaf.Tokens
 import Decaf.AST
-import Dacaf.Scanner
+import Decaf.Scanner
 import Decaf.Parser
 import Decaf.Checker
+import Decaf.Data.Zipper
