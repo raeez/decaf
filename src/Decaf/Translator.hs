ab7dc1a6303a6f65a9e70aa46e426cd3ca4ae70b
Raeez Lorgat <raeez@mit.edu>
2011-03-13 03:34:28 -0400

    Monadify Translator

--- src/Decaf/Translator.hs
+++ src/Decaf/Translator.hs
@@ -31,17 +31,19 @@ instance Monad Translator where
                 let (a, ns) = runTranslator m s
                 in runTranslator (f a) ns)
 
+translate comp ns = fst $ runTranslator comp ns
+
 getNS :: Translator Namespace
 getNS = Translator (\ns -> (ns, ns))
 
-incTemp :: Translator ()
-incTemp = Translator (\(Namespace t l s b) -> ((), Namespace (t+1) l s b))
+incTemp :: Translator Int
+incTemp = Translator (\(Namespace t l s b) -> (t, Namespace (t+1) l s b))
 
 getTemp :: Translator Int
 getTemp = Translator (\ns@(Namespace t _ _ _) -> (t, ns))
 
-incLabel :: Translator ()
-incLabel = Translator (\(Namespace t l s b) -> ((), Namespace t (l+1) s b))
+incLabel :: Translator Int
+incLabel = Translator (\(Namespace t l s b) -> (l, Namespace t (l+1) s b))
 
 getLabel :: Translator Int
 getLabel = Translator (\ns@(Namespace _ l _ _) -> (l, ns))
@@ -65,28 +67,28 @@ getBlock = Translator (\ns@(Namespace _ _ _ b) -> (b, ns))
 -- | Given a SymbolTree, Translate a DecafProgram into an LIRProgram
 translateProgram ::  SymbolTree -> DecafProgram -> Translator LIRProgram
 translateProgram st program =
-    do i <- getTemp
-       incTemp
-       let prog = LIRProgram (LIRLabel $ "L" ++ show i) units
-       return prog
+    do i <- incTemp
+       ns <- getNS
+       return $ LIRProgram (LIRLabel $ "L" ++ show i) (units ns)
   where
-    units = map (translateMethod st) (methods program)
+    units ns = translate (mapM (translateMethod st) (methods program)) ns
 
 -- | Given a SymbolTree, Translate a DecafMethod into an LIRUnit
-translateMethod :: SymbolTree -> DecafMethod -> LIRUnit
+translateMethod :: SymbolTree -> DecafMethod -> Translator LIRUnit
 translateMethod st method =
-    case symLookup (methodID method) (getTable st) of
+    do ns <- getNS
+       return (case symLookup (methodID method) (getTable st) of
         Just (index, MethodRec _ (label, count)) ->
-            LIRUnit (LIRLabel (label ++ show count)) (translateBody (select (rindex index) st)) -- ^ label this method and select the corresponding nested SymbolTree
-        _ -> LIRUnit (LIRLabel ("Translator.hs:71 Invalid SymbolTable; could not find '" ++ methodID method ++ "' symbol")) [] -- ^ should not happen
+            LIRUnit (LIRLabel (label ++ show count)) (translateBody (select (rindex index) st) ns) -- ^ label this method and select the corresponding nested SymbolTree
+        _ -> LIRUnit (LIRLabel ("Translator.hs:71 Invalid SymbolTable; could not find '" ++ methodID method ++ "' symbol")) []) -- ^ should not happen
   where
-    translateBody st' = concatMap (translateStm st') (blockStms $ methodBody method) -- ^ join all translated statements together
+    translateBody st' ns = concat $ translate (mapM (translateStm st') (blockStms $ methodBody method)) ns -- ^ join all translated statements together
     rindex index = (length . children . tree) st - index - 1
 
 -- | Given a SymbolTree, Translate a single DecafStatement into [LIRInst]
-translateStm :: SymbolTree -> DecafStm -> [LIRInst]
+translateStm :: SymbolTree -> DecafStm -> Translator [LIRInst]
 translateStm st (DecafAssignStm loc op expr _) =
-    [lir]
+    return [lir]
   where
     lir = case globalSymLookup (ident loc) st of
               Just (VarRec _ sr) -> LIRRegAssignInst (SREG (show sr)) (LIROperExpr $ translateExpr st expr)
@@ -94,77 +96,81 @@ translateStm st (DecafAssignStm loc op expr _) =
               _ -> LIRLabelInst (LIRLabel $ "Translator.hs:84 Invalid SymbolTable; could not find '" ++ ident loc ++ "' symbol")
 
 translateStm st (DecafMethodStm (DecafPureMethodCall mid args _) _) =
-    [LIRCallInst (LIRCall func (SREG "ip"))]
+    return [LIRCallInst (LIRCall func (SREG "ip"))]
   where
     func = LIRProcLabel (case globalSymLookup mid st of
                             Just (MethodRec _ (label, count)) -> label ++ show count
                             _ -> "Translator.hs:91 Invalid SymbolTable; could not find a valid symbol for'" ++ mid ++ "'")
 
 translateStm st (DecafMethodStm (DecafMethodCallout mid args _) _) =
-    [LIRCallInst (LIRCall func (SREG "ip"))]
+    return [LIRCallInst (LIRCall func (SREG "ip"))]
   where
     func = LIRProcLabel mid
     
 translateStm st (DecafIfStm expr block (Just elseblock) _) =
-    [LIRIfInst (translateRelExpr st expr) truelabel]
-        ++ translateBlock st elseblock
-        ++ [LIRJumpLabelInst endlabel]
-        ++ [LIRLabelInst truelabel]
-        ++ translateBlock st block
-        ++ [LIRLabelInst endlabel]
-  where
-    truelabel = LIRLabel "TRUE"
-    endlabel = LIRLabel "END"
+     do l <- incLabel
+        elseblock <- translateBlock st elseblock
+        trueblock <- translateBlock st block
+        let truelabel = LIRLabel $ "LTRUE" ++ show l
+            endlabel = LIRLabel $ "LEND" ++ show l
+        return ([LIRIfInst (translateRelExpr st expr) truelabel]
+            ++ elseblock
+            ++ [LIRJumpLabelInst endlabel]
+            ++ [LIRLabelInst truelabel]
+            ++ trueblock
+            ++ [LIRLabelInst endlabel])
 
 translateStm st (DecafForStm ident expr expr' block _) =
-    [LIRRegAssignInst (SREG "0") (LIROperExpr $ translateExpr st expr)] -- TODO pullout of st
-        ++ [LIRLabelInst looplabel]
-        ++ [LIRIfInst (translateRelExpr st expr') truelabel]
-        ++ [LIRJumpLabelInst endlabel]
-        ++ [LIRLabelInst truelabel]
-        ++ translateBlock st block
-        ++ [LIRJumpLabelInst looplabel]
-        ++ [LIRLabelInst endlabel]
-  where
-    looplabel = LIRLabel "LOOP"
-    truelabel = LIRLabel "TRUE"
-    endlabel = LIRLabel "END"
-    
+    do  l <- incLabel
+        forblock <- translateBlock st block
+        let looplabel = LIRLabel $ "LOOP" ++ show l
+            truelabel = LIRLabel $ "TRUE" ++ show l
+            endlabel = LIRLabel $ "END" ++ show l
+        return ([LIRRegAssignInst (SREG "0") (LIROperExpr $ translateExpr st expr)] -- TODO pullout of st
+            ++ [LIRLabelInst looplabel]
+            ++ [LIRIfInst (translateRelExpr st expr') truelabel]
+            ++ [LIRJumpLabelInst endlabel]
+            ++ [LIRLabelInst truelabel]
+            ++ forblock
+            ++ [LIRJumpLabelInst looplabel]
+            ++ [LIRLabelInst endlabel])
+
 translateStm st (DecafIfStm expr block Nothing _) =
-    [LIRIfInst (translateRelExpr st expr) truelabel]
-        ++ [LIRJumpLabelInst endlabel]
-        ++ [LIRLabelInst truelabel]
-        ++ translateBlock st block
-        ++ [LIRLabelInst endlabel]
-  where
-    truelabel = LIRLabel "TRUE"
-    endlabel = LIRLabel "END"
+    do l <- incLabel
+       trueblock <- translateBlock st block
+       let truelabel = LIRLabel $ "LTRUE" ++ show l
+           endlabel = LIRLabel $ "LEND" ++ show l
+       return ([LIRIfInst (translateRelExpr st expr) truelabel]
+           ++ [LIRJumpLabelInst endlabel]
+           ++ [LIRLabelInst truelabel]
+           ++ trueblock
+           ++ [LIRLabelInst endlabel])
 
 translateStm st (DecafRetStm (Just expr) _) =
-    [LIRRetOperInst (translateExpr st expr)]
+    return [LIRRetOperInst (translateExpr st expr)]
 
 translateStm st (DecafRetStm Nothing _) =
-    [LIRRetInst]
+    return [LIRRetInst]
 
 translateStm st (DecafBreakStm _) =
-    [LIRJumpLabelInst endlabel]
+    return [LIRJumpLabelInst endlabel]
   where
-    endlabel = LIRLabel "END"
+    endlabel = LIRLabel "TODOBREAKSTATEMENT"
 
 translateStm st (DecafContStm _) =
-    [LIRJumpLabelInst startlabel]
+    return [LIRJumpLabelInst startlabel]
   where
-    startlabel = LIRLabel "LOOP"
+    startlabel = LIRLabel "TODOCONTSTATEMENT"
 
 translateStm st (DecafBlockStm block _) =
     translateBlock st block
 
-translateBlock :: SymbolTree -> DecafBlock -> [LIRInst]
+translateBlock :: SymbolTree -> DecafBlock -> Translator [LIRInst]
 translateBlock st block =
-    translateBody st
+    do ns <- getNS
+       return $ translateBody st ns
   where
-    translateBody st' = concatMap (translateStm st') (blockStms block)
-    rindex index = (length . children . tree) st - index - 1
+    translateBody st' ns = concat $ translate (mapM (translateStm st') (blockStms block)) ns
 
 translateRelExpr :: SymbolTree -> DecafExpr -> LIRRelExpr
 translateRelExpr st expr = LIROperRelExpr $ LIRRegOperand $ RAX
