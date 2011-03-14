a6f3b8cfcbf3e4d01557688de1a88c7145429549
jzz <gnipnuhz@gmail.com>
2011-03-14 00:16:25 -0400

    added 94 new tests

--- testsuite/runtests.hs
+++ testsuite/runtests.hs
@@ -24,6 +24,19 @@ parser_ i = case ps program i of
 semchecker_ :: String -> Bool                      
 semchecker_  = checker 
 
+-- cogen
+cogen_ :: String -> [String]
+cogen_ = ["\t.global main", "main:", "\tret"]
+
+-- cogen legal, returns true if running without runtime error
+cogenl_ :: String -> Bool
+cogenl_ = True
+
+-- cogen content, return program output
+cogenc_ :: String -> String
+cogenc_ = ""
+
+
 
 ------------------------------------
 -- test programs
@@ -41,7 +54,18 @@ readTests names expected func =
     do
       files <- sequence (map readFile names)
       return $TestList.(map (uncurry TestLabel)) $ zip names (map (\cont -> expected ~=? (func cont)) files)
-
+      
+      
+-- read tests and their .out file      : Finish me 
+readTestsWithOut :: [String] -> Bool -> (String -> Bool) -> IO Test
+readTestsWithOut names expected func =
+    do
+      files <- sequence (map readFile $ map (\x -> x++".dcf") names)
+      outs <- sequence (map readFile $ map (\x -> x++".out") names)
+      let tests = map (\x -> snd x ~=? fst x) $ zip files outs 
+      return $TestList.(map (uncurry TestLabel)) $ zip names tests
+      
+      
 
 -- | read test from a list
 readTList :: String -> IO [String]
@@ -72,8 +96,23 @@ main = do
   putStrLn "Semchecker Tests..."
   hiddenSemLegals <- readTests (addPath "semi-legal" $ makeLegals 1 18) True semchecker_
   hiddenSemIllegals <- readTests (addPath "semi-illegal" $ makeIllegals 1 52) False semchecker_
+  tests2 <- readTests (addPath "semi-legal2" $ makeLegals 1 11) True semchecker_
+  testsillegal2 <- readTests (addPath "semi-illegal2" $ makeIllegals 1 62) False semchecker_
   runTestTT hiddenSemLegals
   runTestTT hiddenSemIllegals
+  runTestTT tests2
+  runTestTT testsillegal2
 
 
+  putStrLn "codegen Tests..."
+  cl2 <- readTests (addPath "semi-legal2" $ makeLegals 1 3) True cogenl_
+  cil2 <- readTests (addPath "semi-illegal2" $ makeIllegals 1 4) False cogenl_
+  l <- readTList "coge/l"  
+  cc <- readTestsWithOut (addPath "coge" l) cogenc_  
+  l <- readTList "coge2/l"  
+  cc2 <- readTestsWithOut (addPath "coge2" l) cogenc_  
+  runTestTT cl2
+  runTestTT cil2
+  runTestTT cc
+  runTestTT cc2
 
