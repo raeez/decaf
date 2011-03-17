a7a3599a79507800c5fa23f55507499c8b8c3647
Raeez Lorgat <raeez@mit.edu>
2011-03-17 09:10:20 -0400

    improved compiler binary

--- src/CompilerBin.hs
+++ src/CompilerBin.hs
@@ -4,22 +4,21 @@ import Decaf.Util.InteractiveGrapher
 import Decaf.RegisterAllocator
 import Decaf.IR.ASM
 import System.Environment
+import System.Exit
 
 main = do input <- getArgs
-          let (fileName:fs) = input
-          source <- readFile fileName
-          let (e, p, t, formattedTree, formattedErrors) = getRSuccess $ check source fileName
-          --graph $ generify t
-          if length e > 0 then putStrLn formattedErrors else putStrLn ""
-          let (numberedTable, CounterState (LabelCounter rc _ _ _)) = runRegisterCounter (numberTree $ tree t) (CounterState mkCounter)
-              (lir, _) = runTranslator (translateProgram (top numberedTable) p) (mkNamespace rc)
-          putStrLn $ pp $ translateCFG . convertProgram $ lir
-          let prog = (translateCFG . convertProgram) lir
-              (prog', c, results) = allocateRegisters t prog 
-
-          --putStrLn.pp $ prog' 
-          ---putStrLn.show $ c
-          --mapM (putStrLn.show) results
-          let asm =  ((intelasm . content) numberedTable) ++ (intelasm prog')
-          putStrLn asm
-          writeFile ((fst $ break (=='.') fileName) ++ ".asm") asm
+          let (filename:fs) = input
+          source <- readFile filename
+          do case check source filename of
+              RError s -> putStrLn s
+              RSuccess (e, p, t, formattedTree, formattedErrors) ->
+                  do if length e > 0
+                       then putStrLn formattedErrors >> exitFailure
+                       else do let (numberedTable, CounterState (LabelCounter rc _ _ _)) = runRegisterCounter (numberTree $ tree t) (CounterState mkCounter)
+                                   (lir, _) = runTranslator (translateProgram (top numberedTable) p) (mkNamespace rc)
+                               putStrLn $ pp $ translateCFG . convertProgram $ lir
+                               let prog = (translateCFG . convertProgram) lir
+                                   (prog', c, results) = allocateRegisters t prog 
+                                   asm =  ((intelasm . content) numberedTable) ++ (intelasm prog')
+                               writeFile ((fst $ break (=='.') filename) ++ ".asm") asm
+                               exitSuccess
