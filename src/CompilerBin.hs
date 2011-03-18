d1908e4b42e0d3c0c048d68dfd50ffa532c4b279
Raeez Lorgat <raeez@mit.edu>
2011-03-17 20:00:15 -0400

    improved compiler binary interface

--- src/CompilerBin.hs
+++ src/CompilerBin.hs
@@ -6,19 +6,31 @@ import Decaf.IR.ASM
 import System.Environment
 import System.Exit
 
-main = do input <- getArgs
-          let (filename:fs) = input
-          source <- readFile filename
-          do case check source filename of
-              RError s -> putStrLn s
-              RSuccess (e, p, t, formattedTree, formattedErrors) ->
-                  do if length e > 0
-                       then putStrLn formattedErrors >> exitFailure
-                       else do let (numberedTable, CounterState (LabelCounter rc _ _ _)) = runRegisterCounter (numberTree $ tree t) (CounterState mkCounter)
-                                   (lir, _) = runTranslator (translateProgram (top numberedTable) p) (mkNamespace rc)
-                               putStrLn $ pp $ translateCFG . convertProgram $ lir
-                               let prog = (translateCFG . convertProgram) lir
-                                   (prog', c, results) = allocateRegisters t prog 
-                                   asm =  ((intelasm . content) numberedTable) ++ (intelasm prog')
-                               writeFile ((fst $ break (=='.') filename) ++ ".asm") asm
-                               exitSuccess
+main :: IO ()
+main = do args <- getArgs
+          case args of
+            [s, f] | s == "-debug" ->
+                    do str <- readFile f
+                       compile True str f
+            [f] -> do str <- readFile f
+                      compile False str f
+            _ -> putStrLn "Invalid command line input" >> exitFailure
+
+compile :: Bool -> String -> String -> IO ()
+compile debug source filename =
+  do case check source filename of
+      RError s -> putStrLn s
+      RSuccess (e, p, t, formattedTree, formattedErrors) ->
+          do if length e > 0
+               then putStrLn formattedErrors >> exitFailure
+               else do let (numberedTable, CounterState (LabelCounter rc _ _ _)) = runRegisterCounter (numberTree $ tree t) (CounterState mkCounter)
+                           (lir, _) = runTranslator (translateProgram (top numberedTable) p) (mkNamespace rc)
+                       if debug
+                         then (putStrLn $ pp $ translateCFG . convertProgram $ lir)
+                         else putStrLn ""
+                       let prog = (translateCFG . convertProgram) lir
+                           (prog', c, results) = allocateRegisters t prog 
+                           asm =  ((intelasm . content) numberedTable) ++ (intelasm prog')
+                       writeFile ((fst $ break (=='.') filename) ++ ".asm") asm
+                       putStrLn asm
+                       exitSuccess
