b9e805ddf456eb1e05722c167f580fc9bb1a862f
Scott Kovach <scott@Edwin.(none)>
2011-04-03 23:15:28 -0400

    hoopl core stuff

---
+++ src/Loligoptl/Dataflow.hs
@@ -0,0 +1,337 @@
+{-# LANGUAGE GADTs, EmptyDataDecls, TypeFamilies, RankNTypes, 
+ ScopedTypeVariables, MultiParamTypeClasses #-}
+
+{-
+TODO:
+
+integrate with compiler
+
+-}
+
+module Loligoptl.Dataflow
+where
+
+import Loligoptl.Label
+import Loligoptl.Graph
+
+import Data.Maybe
+
+
+type family   Fact x f :: *
+type instance Fact O f = f
+type instance Fact C f = FactBase f
+
+type Fuel = Int
+class Monad m => FuelMonad m where
+  getFuel :: m Fuel
+  setFuel :: Fuel -> m()
+
+withFuel :: FuelMonad m => Maybe a -> m (Maybe a)
+withFuel Nothing = return Nothing
+withFuel (Just a) = do { f <- getFuel
+                       ; if f == 0
+                         then return Nothing
+                         else setFuel (f-1) >> return (Just a) }
+
+
+data PassDirection = Fwd | Bwd
+
+data FwdPass m n f 
+  = FwdPass { fpLattice :: DataflowLattice f
+            , fpTransfer :: FwdTransfer n f
+            , fpRewrite :: FwdRewrite m n f
+            }
+
+data DataflowLattice f = DataflowLattice
+  { factBottom :: f -- necessary?
+  , factJoin :: f -> f -> (Bool, f)
+  }
+
+-- are these foralls needed?
+newtype FwdTransfer n f = FwdTransfer3 
+  { getFwdTransfer3 :: 
+      ( n C O -> f -> f
+      , n O O -> f -> f
+      , n O C -> f -> FactBase f
+      )
+  }
+
+
+newtype FwdRewrite m n f = FwdRewrite3
+  { getFwdRewrite3 :: 
+      ( n C O -> f -> m (Maybe (FwdRev m n f C O))
+      , n O O -> f -> m (Maybe (FwdRev m n f O O))
+      , n O C -> f -> m (Maybe (FwdRev m n f O C))
+      )
+  }
+
+-- fwd revision to graph
+-- includes the new subgraph, and a new rewrite function to use when analyzing the new graph
+data FwdRev m n f e x  = FwdRev (Graph n e x) (FwdRewrite m n f)
+
+
+analyzeAndFwdRewrite
+  :: (FuelMonad m, NonLocal n)
+  => FwdPass m n f
+  -> [Label]          -- entry points
+  -> Graph n C C      -- might need to make more polymorphic on shape 
+  -> FactBase f       -- input facts
+  -> m (Graph n C C, FactBase f)
+
+analyzeAndFwdRewrite pass entryLabels g fb = 
+  do { (dg, f) <- afrGraph pass entries g fb
+     ; return $ normalizeGraph dg }
+  where entries = 
+          case entryLabels of 
+            [] -> error "closed graph not provided any entry points" -- This is an error I think
+            otherwise -> JustC entryLabels
+                    
+
+normalizeGraph :: NonLocal n => DG f n e x -> (Graph n e x, FactBase f)
+normalizeGraph g = (dropFacts g, facts g)
+  where
+    dropFacts GNil = GNil
+    dropFacts (GUnit (DBlock f b)) = b
+    dropFacts (GMany e b x) = GMany (fmap dropFact e) (mapMap dropFact b) (fmap dropFact x)
+      where dropFact (DBlock _ b) = b
+
+    facts :: DG f n e x -> Graph n e x
+    facts GNil = mapEmpty
+    facts (GUnit _) = mapEmpty -- not sure why this should be empty
+    facts (GMany _ body exit) = bodyFacts body `mapUnion` exitFacts exit
+    bodyFacts body = mapFold pullFact mapEmpty body
+      where pullFact (DBlock f b) fb = mapInsert (entryLabel b) f fb
+    exitFacts :: MaybeO x (DBlock f n C O) -> FactBase f
+    exitFacts NothingO = mapEmpty
+    exitFacts (JustO (DBlock f b)) = mapSingleton (entryLabel b) f
+
+afrGraph
+  :: forall m n f e x . (FuelMonad m, NonLocal n)
+  => FwdPass m n f
+  -> MaybeC e [Label]  -- entry points if graph is closed
+  -> Graph n e x
+  -> Fact e f          -- facts flowing in
+  -> m (DG f n e x, Fact x f)
+
+afrGraph pass entries = graph
+  where
+    node :: forall e x . (ShapeLifter e x) => n e x     
+         ->  f -> m (DG f n e x, Fact x f)
+
+    node n f = 
+        do mrev <- frewrite pass n f >> withFuel
+           case mrev of 
+             Nothing -> return (singletonDG f n, ftransfer pass n f)
+             Just (FwdRev g rw) ->
+                 let pass' = pass {fpRewrite = rw}
+                     f' = fwdEntryFact n f
+                 in afrGraph pass' (fwdEntryLabel n) g f'
+
+    block :: forall e x. Block n e x 
+          -> f -> m (DG f n e x, Fact x f)
+    block (BFirst  n)  = node n
+    block (BMiddle n)  = node n
+    block (BLast   n)  = node n
+
+    block (BCat b1 b2) = block b1 `cat` block b2
+
+    block (BHead h n)  = block h  `cat` node n
+    block (BTail n t)  = node  n  `cat` block t
+    block (BClosed h t)= block h  `cat` block t
+      where
+        cat t1 t2 f = do { (g1, f1) <- t1 f
+                         ; (g2, f2) <- t2 f1
+                         ; return (g1 `dgSplice` g2, f2) }
+
+    body :: [Label] -> LabelMap (Block n C C) 
+         -> Fact C f  -> m (DG f n e x, Fact C f) 
+
+    body entries blockmap initFBase
+        = fixpoint Fwd lattice doBlock blocks initFBase
+      where
+          blocks = forwardBlockList entries blockmap
+          lattice = fpLattice pass
+          doBlock b fb = block b entryFact
+            where entryFact = getFact lattice (entryLabel b) fb
+                                
+
+    graph :: Graph n e x 
+          -> Fact e f -> m (DG f n e x, Fact x f)
+    graph GNil            = \f -> return (dgNil, f)
+    graph (GUnit blk)     = block blk
+    graph (GMany e bdy x) = (e `ebcat` bdy) `cat` exit x
+     where
+      ebcat :: MaybeO e (Block n O C) -> Body n -> Fact e f -> m (DG f n e C, Fact C f)
+      exit  :: MaybeO x (Block n C O)           -> Fact C f -> m (DG f n C x, Fact x f)
+      exit (JustO blk) = arfx block blk
+      exit NothingO    = \fb -> return (dgNilC, fb)
+      ebcat entry bdy = c entries entry
+       where c :: MaybeC e [Label] -> MaybeO e (Block n O C)
+                -> Fact e f -> m (DG f n e C, Fact C f)
+             c NothingC (JustO entry)   = block entry `cat` body (successors entry) bdy
+             c (JustC entries) NothingO = body entries bdy
+             c _ _ = error "bogus GADT pattern match failure"
+
+    arfx :: forall thing x . NonLocal thing
+         => (thing C x ->        f -> m (DG f n C x, Fact x f))
+         -> (thing C x -> Fact C f -> m (DG f n C x, Fact x f))
+    arfx arf thing fb = 
+      arf thing $ fromJust $ mapLookup (entryLabel thing) $ joinInFacts lattice fb
+     where lattice = fpLattice pass
+
+
+
+
+
+data ChangeFlag = SomeChange | NoChange
+data FixState n f 
+  = FS { fsChange :: ChangeFlag
+       , fsFBase :: FactBase f
+       , fsDG :: DG f n C C
+       , fsLabels :: LabelSet
+       }
+
+updateFact :: DataflowLattice f -> LabelSet 
+           -> Label -> f -> (ChangeFlag, FactBase f)
+           -> (ChangeFlag, FactBase f)
+updateFact lat labels label newFact (ch, fbase)
+  | NoChange <- ch' = (ch, fbase)
+  | label `setMember` labels = (SomeChange, newFBase)
+  | otherwise = (ch, newFBase)
+  where
+    (ch', resFact) 
+       = case mapLookup label fbase of
+           Nothing -> (SomeChange, new_fact_debug)
+           Just oldFact -> join oldFact
+         where join oldFact = 
+                 factJoin lat label oldFact newFact
+               (_, new_fact_debug) = join (factBottom lat)
+    newFBase = mapInsert label resFact fbase
+  
+
+fixpoint :: PassDirection -> DataflowLattice f
+         -> (Block n e x -> FactBase f -> m (DG f n e x, Fact x f)) -- might need forall as in block
+         -> [Block n e x] -> FactBase f ->  m (DG f n e x, Fact C f)
+
+fixpoint dir lattice doBlock blocks initFBase
+  = do { fs <- loop initFBase
+       -- return the dg produced, and the part of the factbase
+       -- consisting of labels NOT in the subgraph being considered
+       ; return (fsDG fs, mapDeleteList (map (fst . fst) taggedBlocks) (fsFBase fs))
+       }
+  where
+    loop fbase = 
+      do{ fs <- iterBlocks taggedBlocks (FS {fsChange = NoChange, fsFBase = fbase
+                                          , fsDG = dgNilC, fsLabels = setEmpty})
+        ; case fsChange fs of 
+            NoChange -> return fs
+            SomeChange -> loop $ fsFBase fs }
+
+    taggedBlocks = map tag blocks
+    tag :: NonLocal t => t C C -> ((Label, t C C), [Label])
+    tag b = ((entryLabel b, b), 
+             if (dir == Fwd) then [entryLabel b] 
+             else successors b)
+    iterBlocks :: [((Label, Block n C C), [Label])]
+               -> FixState n f -> m (FixState n f)
+    iterBlocks (((l, b), entries):bs) fs 
+      = iterBlock l b entries fs >>= iterBlocks bs
+
+    iterBlock :: Label -> Block n C C -> [Label] -> FixState n f -> m (FixState n f)
+    iterBlock label block inLabels
+      fs@(FS {fsChange = ch, fsFBase = fbase, fsDG = dg, fsLabels = labels})
+        | dir == Fwd  && not (label `mapMember` fbase)
+          = return $ fs {fsLabels = labels'}
+        | otherwise
+          = do { (dg', outFacts) <- doBlock block fbase
+               ; let (ch', fbase') = mapFoldWithKey
+                                     (updateFact lattice labels) 
+                                     (ch,fbase) outFacts
+               ; return $ 
+                        FS { fsChange = ch'
+                           , fsFBase = fbase'
+                           , fsDG = dg `dgSplice` dg'
+                           , fsLabels = labels' } }
+        where
+          labels' = labels `setUnion` setFromList inLabels
+      
+
+
+
+          
+       
+
+-- | Orders the blocks in a graph for data flow analysis.
+-- | Could make this faster using folds
+forwardBlockList :: [Label] -> LabelMap (Block n C C) -> [Block n C C]
+forwardBlockList entries body
+  = concatMap traverse entries -- only takes blocks reachable from entries
+  where traverse label = block : concatMap traverse (successors block)
+          where block = (mapLookup label body)                                                     
+
+                                                   
+joinInFacts :: DataflowLattice f -> FactBase f -> FactBase f
+joinInFacts (lattice @ DataflowLattice {factBottom = bot, factJoin = fj}) fb =
+  mkFactBase lattice $ map botJoin $ mapToList fb
+    where botJoin (l, f) = (l, snd $ fj l bot f)
+                        
+
+
+
+class ShapeLifter e x where
+ singletonDG   :: f -> n e x -> DG f n e x
+ fwdEntryFact  :: NonLocal n => n e x -> f -> Fact e f
+ fwdEntryLabel :: NonLocal n => n e x -> MaybeC e [Label]
+ ftransfer :: FwdPass m n f -> n e x -> f -> Fact x f
+ frewrite  :: FwdPass m n f -> n e x 
+           -> f -> m (Maybe (FwdRev m n f e x))
+{- bwdEntryFact :: NonLocal n => DataflowLattice f -> n e x -> Fact e f -> f
+ btransfer    :: BwdPass m n f -> n e x -> Fact x f -> f
+ brewrite     :: BwdPass m n f -> n e x
+              -> Fact x f -> m (Maybe (Graph n e x, BwdRewrite m n f))
+-}
+instance ShapeLifter C O where
+  singletonDG f = gUnitCO . DBlock f . BFirst
+  fwdEntryFact     n f  = mapSingleton (entryLabel n) f
+--  bwdEntryFact lat n fb = getFact lat (entryLabel n) fb
+  ftransfer (FwdPass {fpTransfer = FwdTransfer3 (ft, _, _)}) n f = ft n f
+--  btransfer (BwdPass {bpTransfer = BwdTransfer3 (bt, _, _)}) n f = bt n f
+  frewrite  (FwdPass {fpRewrite  = FwdRewrite3  (fr, _, _)}) n f = fr n f
+--  brewrite  (BwdPass {bpRewrite  = BwdRewrite3  (br, _, _)}) n f = br n f
+  fwdEntryLabel n = JustC [entryLabel n]
+
+instance ShapeLifter O O where
+  singletonDG f = gUnitOO . DBlock f . BMiddle
+  fwdEntryFact   _ f = f
+  --bwdEntryFact _ _ f = f
+  ftransfer (FwdPass {fpTransfer = FwdTransfer3 (_, ft, _)}) n f = ft n f
+--  btransfer (BwdPass {bpTransfer = BwdTransfer3 (_, bt, _)}) n f = bt n f
+  frewrite  (FwdPass {fpRewrite  = FwdRewrite3  (_, fr, _)}) n f = fr n f
+--  brewrite  (BwdPass {bpRewrite  = BwdRewrite3  (_, br, _)}) n f = br n f
+  fwdEntryLabel _ = NothingC
+
+instance ShapeLifter O C where
+  singletonDG f = gUnitOC . DBlock f . BLast
+  fwdEntryFact   _ f = f
+--  bwdEntryFact _ _ f = f
+  ftransfer (FwdPass {fpTransfer = FwdTransfer3 (_, _, ft)}) n f = ft n f
+--  btransfer (BwdPass {bpTransfer = BwdTransfer3 (_, _, bt)}) n f = bt n f
+  frewrite  (FwdPass {fpRewrite  = FwdRewrite3  (_, _, fr)}) n f = fr n f
+--  brewrite  (BwdPass {bpRewrite  = BwdRewrite3  (_, _, br)}) n f = br n f
+  fwdEntryLabel _ = NothingC
+
+getFact  :: DataflowLattice f -> Label -> FactBase f -> f
+getFact lat l fb = case mapLookup l fb of Just  f -> f
+                                          Nothing -> factBottom lat
+
+mkFactBase :: DataflowLattice f -> [(Label, f)] -> FactBase f
+mkFactBase lattice = foldl add mapEmpty
+  where add :: FactBase f -> (Label, f) -> FactBase f
+        add map (label, f) = mapInsert label newFact map
+          where newFact = case mapLookup label map of
+                            Nothing -> f
+                            Just f' -> snd $ join label f' f
+                join = factJoin lattice
+
+
+main = putStrLn "1"
---
+++ src/Loligoptl/Label.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE GADTs, EmptyDataDecls, TypeFamilies, TypeSynonymInstances #-}
+module Loligoptl.Label
+  ( Label
+  , allLabels -- to be used only by the Fuel monad
+  , LabelMap
+  , FactBase
+  , LabelSet
+  , IsMap (..)
+  , IsSet (..)
+  , mapDeleteList, mapMember
+  )
+
+where
+
+import qualified Data.IntMap as M
+import qualified Data.IntSet as S
+
+newtype Label = Label { unLabel :: Int }
+  deriving (Eq, Ord)
+
+instance Show Label where
+  show (Label n) = "L" ++ show n
+
+allLabels :: [Label]
+allLabels = map Label [1..]
+
+newtype LabelMap a = LM (M.IntMap a)
+type FactBase a = M.IntMap a
+newtype LabelSet = LS (S.IntSet)
+
+class IsMap map where
+  type KeyOf map
+
+  mapEmpty :: map a
+  mapLookup :: KeyOf map -> map a -> Maybe a
+  mapInsert :: KeyOf map -> a -> map a -> map a
+  mapDelete :: KeyOf map -> map a -> map a
+  mapUnionWithKey :: ((KeyOf map) -> a -> a -> a) -> map a -> map a -> map a
+  mapFold :: (a -> b -> b) -> b -> map a -> b
+  mapFoldWithKey :: (KeyOf map -> a -> b -> b) -> b -> map a -> b
+  mapToList :: map a -> [(KeyOf map, a)]
+  mapSingleton :: KeyOf map -> a -> map a
+  mapMap :: (a->b) -> map a -> map b
+  mapUnion :: map a -> map a -> map a
+
+-- utility functions defined on maps
+mapDeleteList :: (IsMap map) => [KeyOf map] -> map a -> map a
+mapDeleteList keys map = foldl (flip mapDelete) map keys -- fold the deletes together
+
+mapMember :: (IsMap map) => KeyOf map -> map a -> Bool
+mapMember key map = case mapLookup key map of
+                      Just _ -> True
+                      Nothing -> False
+
+class IsSet set where
+  type ElemOf set
+
+  setEmpty :: set
+  setUnion :: set -> set -> set
+  setFromList :: [ElemOf set] -> set
+  setMember :: ElemOf set -> set -> Bool
+
+instance IsMap LabelMap where
+  type KeyOf LabelMap = Label
+  
+  mapEmpty = LM (M.empty)
+  mapLookup label (LM m) = M.lookup (unLabel label) m
+  mapInsert key val (LM m) = LM (M.insert (unLabel key) val m)
+  mapDelete key (LM m) = LM (M.delete (unLabel key) m)
+  mapUnionWithKey f (LM m1) (LM m2) = LM (M.unionWithKey (f . Label) m1 m2)
+  mapFold f b (LM m) = M.fold f b m
+  mapFoldWithKey f b (LM m) = M.foldWithKey (f . Label) b m
+  mapToList (LM m) = map (\(k,a) -> (Label k, a)) $ M.toList m
+  mapSingleton key a = LM (M.singleton (unLabel key) a)
+  mapMap f (LM m) = LM (M.map f m)
+  mapUnion (LM m1) (LM m2) = LM (M.union m1 m2)
+
+instance IsSet LabelSet where
+  type ElemOf LabelSet = Label
+  
+  setUnion (LS s1) (LS s2) = LS (S.union s1 s2)
+  setEmpty = LS S.empty
+  setFromList list = LS (S.fromList (map unLabel list))
+  setMember a (LS s) = S.member (unLabel a) s
