95e5edef34ddbdf26719aaa5bc1a4142180e152e
jzz <gnipnuhz@gmail.com>
2011-04-15 01:27:58 -0400

    make TRCSE.hs compile  (within basic block CSE)

--- src/Decaf/TRCSE.hs
+++ src/Decaf/TRCSE.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE GADTs, RankNTypes, ScopedTypeVariables, NoMonomorphismRestriction #-}
+
 module Decaf.TRCSE where
-import Compiler.Hoopl hiding (Top)
 import Data.Map as Map
 import Decaf.IR.LIR
 import Control.Monad
 import Decaf.HooplNodes
+import Loligoptl.Dataflow
+import Loligoptl.Graph 
+import Loligoptl.Fuel 
+import Loligoptl.Label 
+import Loligoptl.Combinators
+import Data.Maybe
 
 
 ----------------------------------------------------
@@ -20,19 +27,21 @@ import Decaf.HooplNodes
 
 
 
+-- CSE data structure, maps (Opr, Op, Opr) key to Var 
 data CSEKey = CSEKey LIROperand LIRBinOp LIROperand
+               deriving (Show, Eq)
 type CSEData = LIRReg
 type CSERecord = (CSEKey, CSEData)
-              deriving (Show, Eq, Typeable)
+
 
 type CSELattice = [CSERecord]
 
 
 
+
 -- join two CSE lattices : fix me!     (right now, the joined results is always empty, CSE is completely local to basic block)
-joinCSELattice :: JoinFun CSELattice
-joinCSELattice _ (OldFact m) (NewFact n) = 
-  (NoChange, []);
+joinCSELattice :: CSELattice -> CSELattice -> (ChangeFlag, CSELattice)
+joinCSELattice _ _ = (NoChange, [])
         
 
   
@@ -40,52 +49,54 @@ joinCSELattice _ (OldFact m) (NewFact n) =
 -- define const lattice
 constLattice :: DataflowLattice CSELattice
 constLattice = DataflowLattice
-  { fact_name   = "CSElattice"
-  , fact_bot    = empty        
-  , fact_join   = joinCSELattice }
+  { factBottom = []
+  , factJoin   = joinCSELattice }
 
 
 
--- remvoe all records containing x, because x has been changed
+-- aux: remvoe all records containing x, because x has been changed
 varChanged :: CSELattice -> LIRReg -> CSELattice
-varChanged lat x = filter ok lat
-                   where 
-                     -- a record is ok (to keep) if it does not contain x
-                     ok :: CSERecord -> Bool
-                     ok (CSEKey a op b, c) = (oprMismatch a) && (oprMismatch b) && (c /= x)
+varChanged lat x = Prelude.filter ok lat
+  where 
+    -- a record is ok (to keep) if it does not contain x
+    ok :: CSERecord -> Bool
+    ok (CSEKey a op b, c) = (oprMismatch a) && (oprMismatch b) && (c /= x)
                      
-                     -- a operand does not contains x if it is not a RegOperand made with x
-                     oprMismatch :: LIROperand -> Bool
-                     oprMismatch (LIRRegOperand x') = (x /= x')  -- if it is a RegOperand, then change x /= x'
-                     oprMismatch _                  = True       -- mismatch always holds otherwise
+    -- a operand does not contains x if it is not a RegOperand made with x
+    oprMismatch :: LIROperand -> Bool
+    -- if it is a RegOperand, then change x /= x'
+    oprMismatch (LIRRegOperand x') = (x /= x')  
+    -- mismatch always holds otherwise
+    oprMismatch _                  = True       
                      
 
 
 
 
--- lookup expression 
+-- aux: lookup expression 
 lookupExpr :: CSELattice -> CSEKey -> Maybe LIRReg 
 lookupExpr lat k = 
-  let recs = filter (\(x,y) -> if x == k then True else False) lat   -- filter out only expression with key match
-  in  if recs == [] then Nothing else Just $ snd $ recs !! 0         -- pick the first match
+  -- filter out only expression with key match
+  let recs = Prelude.filter (\(x,y) -> if x == k then True else False) lat   
+  -- pick the first match
+  in  if recs == [] then Nothing else Just $ snd $ recs !! 0         
 
 
 
 
 
--- define CSE lattice transfer function:  a expression (x op y) is available iff ((x,op,y),w) \in Lattice
+-- transfer: define CSE lattice transfer function:  a expression (x op y) is available iff ((x,op,y),w) \in Lattice
 -- concerns only RegAssignNode and LoadNode
 exprIsAvail :: FwdTransfer Node CSELattice
 exprIsAvail = mkFTransfer ft
   where
     ft :: Node e x -> CSELattice -> Fact x CSELattice
     ft (LIRLabelNode _) f = f
-    ft (LIRRegAssignNode x (LIRBinExpr a op b)) f = ((a, op, b), x) : (varChanged f x)  -- a op b -> x
+    ft (LIRRegAssignNode x (LIRBinExpr a op b)) f = ((CSEKey a op b), x) : (varChanged f x)  -- a op b -> x
     ft (LIRRegAssignNode x _) f = f                                                     -- x = _, no change
-    ft (LIRJumpLabelNode l) f = mkFactBase [(l, f)]                    -- jmp l --> associate f with l 
-    ft (LIRIfNode expr tl fl) f = mkFactBase [(tl, f), (fl, f)]        -- if expr the jmp tl else jmp fl
+    ft (LIRJumpLabelNode l) f = mkFactBase constLattice [(l, f)]                    -- jmp l --> associate f with l 
+    ft (LIRIfNode expr tl fl) f = mkFactBase constLattice [(tl, f), (fl, f)]        -- if expr the jmp tl else jmp fl
     
-    ft (LIRTempEnterNode _) f = f
     ft (LIRRegOffAssignNode _ _ _ _) f = f 
     ft (LIRStoreNode _ _) f = f
     ft (LIRLoadNode x _) f = varChanged f x          -- remove all records containing x
@@ -100,23 +111,25 @@ exprIsAvail = mkFTransfer ft
 
 
 
--- define constant folding rewrites
-cse :: Monad m => FwdRewrite m Node f
-cse = deepFwdRw simp
+-- rewrite: define constant folding rewrites
+cse :: Monad m => FwdRewrite m Node CSELattice
+cse = shallowFwdRw simp
   where
-    simp :: Node -> Fact x CSELattice -> m (Maybe (Graph n e x))
-    simp node _ = return $ liftM nodeToG $ s_node node
+    simp :: forall m e x . (ShapeLifter e x, Monad m) => 
+            Node e x -> CSELattice -> m (Maybe (Graph Node e x))
+    simp node f = return $ liftM nodeToG $ s_node node
   
-    s_node :: Node e x -> Maybe (Node e x)
-    
-    -- x = lit op lit     
-    s_node (LIRRegAssignNode x (LIRBinExpr o1@(LIRRefOperand r1) op o2@(LIRRegOperand r2))) = 
-      case lookupExpr f $ CSEKey o1 op o2 of
-        Just a  -> Just $ LIRRegAssignNode x $ LIROperExpr $ LIRRegOperand a   -- 
-        Nothing -> Nothing
+      where
+        s_node :: Node e x -> Maybe (Node e x)
+        -- x = lit op lit     
+        s_node (LIRRegAssignNode x (LIRBinExpr o1@(LIRRegOperand r1) 
+                                           op o2@(LIRRegOperand r2))) = 
+          case lookupExpr f $ CSEKey o1 op o2 of
+            Just a  -> Just $ LIRRegAssignNode x $ LIROperExpr $ LIRRegOperand a  
+            Nothing -> Nothing
 
-    -- others do not need CSE
-    s_node _ = Nothing
+        -- others do not need CSE
+        s_node _ = Nothing
 
 
 
@@ -126,7 +139,7 @@ cse = deepFwdRw simp
 
 -- define fwd pass
 csePass = FwdPass
-  { fp_lattice = CSELattice
-  , fp_transfer = exprIsAvail
-  , fp_rewrite = cse }
+  { fpLattice = constLattice
+  , fpTransfer = exprIsAvail
+  , fpRewrite = cse }
 
